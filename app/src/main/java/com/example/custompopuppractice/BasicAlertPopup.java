669fba998d6298bdd5b46f84c6644b7c0aaee94b
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-31 23:32:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/custompopuppractice/BasicAlertPopup.java
@@ -0,0 +1,14 @@
+package com.example.custompopuppractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicAlertPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_alert_popup);
+    }
+}
